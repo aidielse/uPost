fd13762d97d48073889dcb0e8c2a5dcb82ede2a9
mikeogod <mikeogod@gmail.com>
2014-02-19 18:26:54 -0500

    recreated as php project, and added bootstrap and jquery

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+  		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  		<meta name="viewport" content="width=device-width, initial-scale=1">
+  		
+  		<!-- Jquery -->
+  		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  		
+  		<!-- Bootstrap -->
+  		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+  		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
+  		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+  		
+  		
+		<title> uPost Social Network Update Manager</title>
+	</head>
+	<body>
+		<div class="container">
+			<h1> uPost will be put here, this is just a palceholder </h1>
+		</div>
+	</body>
+</html>
