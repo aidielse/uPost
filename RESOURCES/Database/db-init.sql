78f00fb0ebeabe833b840470a91a5db8096acadb
root <root@localhost.localdomain>
2014-02-25 18:45:49 -0500

    added Entity Relationship Diagram for database, added Relational model of database, added db-init.sql file

--- RESOURCES/Database/db-init.sql
+++ RESOURCES/Database/db-init.sql
@@ -2,11 +2,28 @@ DROP DATABASE IF EXISTS upost;
 
 CREATE DATABASE upost;
 
-DROP DATABASE IF EXISTS users CASCADE;
+USE upost;
+
+DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
 
 	userId INT PRIMARY KEY AUTO_INCREMENT,
 	userName VARCHAR(255) UNIQUE NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
 	password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE sessions (
+
+	sessionId INT PRIMARY KEY AUTO_INCREMENT,
+	userId INT,
+	token VARCHAR(255),
+	tokenIdentifier VARCHAR(1),
+
+	FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE
+);
+
+GRANT ALL ON upost.* to 'phpuser1'@'localhost';
+
+SET PASSWORD FOR 'phpuser1'@'localhost' = PASSWORD('phppass');
