27db2a38d4cf6ec2ae981fa250f9859530e04e0b
mikeogod <mikeogod@gmail.com>
2014-03-16 10:45:36 -0400

    316/2014

---
+++ account.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+  		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  		<meta name="viewport" content="width=device-width, initial-scale=1">
+  		
+  		<style>
+  		
+  		</style>
+  		
+  		<!-- Jquery -->
+  		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  		
+  		<!-- Bootstrap -->
+  		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+  		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
+  		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+  		<link rel="stylesheet" href="Styles/general.css">
+  		
+		<title> Manage your account</title>
+	</head>
+	
+	<?php //if (isset($_SESSION["user"])):?>
+	<body>
+		<div class="fluid-container">
+		    <div class="navbar navbar-default fixed-top" role="navigation">
+		        <div class="navbar-header">
+		            <a class="navbar-brand" href="index.php">uPOST</a>
+		          	
+		        </div>
+		        
+		        <ul class="nav navbar-nav navbar-right">
+		        	<li class="dropdown">
+		        		<!-- Generated dynamically -->
+		        		<a href="#" class="dropdown-toggle" data-toggle="dropdown">Username <b class="caret"></b></a>
+		        	    <ul class="dropdown-menu">
+		        	    	<li><a href="#contactus">change username</a></li>
+		        	    	<li><a href="#github">change password</a></li>
+		        	    </ul>
+          			</li>
+          			<li class="dropdown">
+          				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Accounts <b class="caret"></b></a>
+          				<ul class="dropdown-menu">
+          					<!-- Generated dynamically -->
+          					<li><li>
+          					<li><li>
+          					<li><li>
+          				</ul>
+          			</li>
+		        </ul>
+		        
+		    </div>
+		</div>
+		
+		<div class="fluid-container">
+			<div class="row">
+			    <div class="col-md-2 hidden-sm hidden-xs"></div>
+			    <div class="col-md-8 panel custom-panel">
+			    	<form role="form">
+			    		<div class="form-group">
+			    			<label for="change_password">Change your username: </label>
+			    			<input id="change_username" class="form-control" type="text" required>
+			    		</div>
+			    		<div class="form-group">
+			    			<input class="btn btn-default" type="submit" value="OK">
+			    		</div>
+			    	</form>
+			    	<form role="form">
+			    		<div class="form-group">
+			    			<label for="change_password">Change your password: </label>
+			    			<input id="change_password" class="form-control" type="password" required>
+			    			<label for="change_password_again">Type it again: </label>
+			    			<input id="change_password_again" class="form-control" type="password" required>
+			    			<label for="old_password">Type your old password: </label>
+			    			<input id="old_password" class="form-control" type="password" required>
+			    		</div>
+			    		<div class="form-group">
+			    			<input class="btn btn-default" type="submit" value="OK">
+			    		</div>
+			    	</form>
+			    </div>
+			    <div class="col-md-2 hidden-sm hidden-xs"></div>
+			</div>
+			<div class="row">
+				<div class="col-md-2 hidden-sm hidden-xs"></div>
+				<div class="col-md-8 panel custom-panel">
+					<div class="row">
+						<div class="col-md-3">
+							<p>Social Network Site 1</p>
+						</div>
+						<div class="col-md-6">
+							<form role="form">
+								<div class="form-group">
+									<label>Username:</label>
+									<input class="form-control" type="text">
+								</div>
+								<div class="form-group">
+									<label>Password:</label>
+									<input class="form-control" type="password">
+								</div>
+								<div class="form-group">
+									<input class="btn btn-default btn-block" type="submit" value="login">
+								</div>
+							</form>
+						</div>
+						<div class="col-md-3">
+							<p>Login status:</p>
+							<p>Logged in</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2 hidden-sm hidden-xs"></div>
+			</div>
+			<div class="row">
+				<div class="col-md-2 hidden-sm hidden-xs"></div>
+				<div class="col-md-8 panel custom-panel">
+					<div class="row">
+						<div class="col-md-3">
+							<p>Social Network Site 1</p>
+						</div>
+						<div class="col-md-6">
+							<form role="form">
+								<div class="form-group">
+									<label>Username:</label>
+									<input class="form-control" type="text">
+								</div>
+								<div class="form-group">
+									<label>Password:</label>
+									<input class="form-control" type="password">
+								</div>
+								<div class="form-group">
+									<input class="btn btn-default btn-block" type="submit" value="login">
+								</div>
+							</form>
+						</div>
+						<div class="col-md-3">
+							<p>Login status:</p>
+							<p>Logged in</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2 hidden-sm hidden-xs"></div>
+			</div>
+		</div>
+	</body>
--- index.php
+++ index.php
@@ -6,34 +6,7 @@
   		<meta name="viewport" content="width=device-width, initial-scale=1">
   		
   		<style>
-  		.icon-bar{
-  			display: block;
-			width: 22px;
-			height: 2px;
-			border-radius: 1px;
-			background-color: #888;
-			margin-top: 4px;
-  		}
   		
-  		.custom-panel{
-  			padding: 10px;
-      		border-radius: 0px;
-      		box-shadow:5px 5px 5px grey;
-      		border:solid #e7e7e7 1px;
-      		text-align:center;
-      		-webkit-transition: -webkit-transform 1s, box-shadow 1s;
-      		transition: transform 1s, box-shadow 1s;
-      		
-  		}
-  		.custom-panel:not(:first-child){
-  			margin-top: 30px;
-  		}
-  		
-  		.custom-panel:hover{
-  			box-shadow:0px 0px 0px grey;
-  			-webkit-transform: translate(5px,8px);
-  			transform: translate(5px, 8px);
-  		}
   		</style>
   		
   		<!-- Jquery -->
@@ -44,10 +17,11 @@
   		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
   		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
   		
+  		<link rel="stylesheet" href="Styles/general.css">
   		
 		<title> uPost Social Network Update Manager</title>
 	</head>
-
+<?php $_SESSION["user"]="Anonymous"; ?>
 <?php if(!isset($_SESSION["user"])): ?>
 	<body>
 		<div class="fluid-container">
@@ -76,7 +50,7 @@
 		<div class="fluid-container">
 			<div class="row">
 			    <div class="col-md-2 hidden-sm hidden-xs"></div>
-			    <div class="col-md-8 .panel custom-panel">
+			    <div class="col-md-8 panel custom-panel">
 			    	<h1>What is uPOST?</h1>
 			    	<p>Focus on what and where to post. uPost takes care of the rest.</p>
 					<p>uPost is a web application via which users can post content to multiple social media.</p>
@@ -86,7 +60,7 @@
 			</div>
 			<div class="row" id="contactus">
 				<div class="col-md-2 hidden-sm hidden-xs"></div>
-			    <div class="col-md-8 .panel custom-panel">
+			    <div class="col-md-8 panel custom-panel">
 			    	<h1>Contact Us: </h1>
 			    	<p>Aaron Sedlacek: <a href="mailto:aarons104@gmail.com">aarons104@gmail.com</a></p>
 			    	<p>Evan MacGregor: <a href="mailto:macgregor.evan@gmail.com">macgregor.evan@gmail.com</a></p>
@@ -98,7 +72,7 @@
 			</div>
 			<div class="row" id="contactus">
 				<div class="col-md-2 hidden-sm hidden-xs" id="github"></div>
-			    <div class="col-md-8 .panel custom-panel">
+			    <div class="col-md-8 panel custom-panel">
 			    	<h1>Follow Us on Github: </h1>
 			    	<p>Github Repo: <a href="https://github.com/Aarons100/uPost">https://github.com/Aarons100/uPost</a></p>
 			    	<br />
@@ -111,8 +85,61 @@
 	</body>
 <?php else: ?>
 	<body>
-		<div>
-			<p>You are logged in!</p>
+		<div class="fluid_container">
+			<div class="navbar navbar-default fixed-top" role="navigation">
+		        <div class="navbar-header">
+		            <a class="navbar-brand" href="index.php">uPOST</a>
+		          	
+		        </div>
+		        
+		        <ul class="nav navbar-nav navbar-right">
+		        	<li class="dropdown">
+		        		<!-- Generated dynamically -->
+		        		<a href="#" class="dropdown-toggle" data-toggle="dropdown">Username <b class="caret"></b></a>
+		        	    <ul class="dropdown-menu">
+		        	    	<li><a href="#contactus">change username</a></li>
+		        	    	<li><a href="#github">change password</a></li>
+		        	    </ul>
+          			</li>
+          			<li class="dropdown">
+          				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Accounts <b class="caret"></b></a>
+          				<ul class="dropdown-menu">
+          					<!-- Generated dynamically -->
+          					<li><li>
+          					<li><li>
+          					<li><li>
+          				</ul>
+          			</li>
+		        </ul>
+		    </div>
+		</div>
+		
+		<div class="fluid-container">
+			<div class="row">
+			    <div class="col-md-2 hidden-sm hidden-xs"></div>
+			    <div class="col-md-8 panel custom-panel">
+			    	<form role="form">
+			    		<div class="form-group">
+			    			<label for="post_text">Write something and public everywhere!</label>
+			    			<textarea id="post_text" class="form-control" placeholder="Write something you want to say..." rows="5"></textarea>
+			    			
+			    		</div>
+			    		<div class="form-group">
+			    			<!-- Generated dynamically -->
+			    			<label class="checkbox-inline">
+			    				Facebook<input type="checkbox">
+			    			</label>
+			    			<label class="checkbox-inline">
+			    				Twitter<input type="checkbox">
+			    			</label>
+			    		</div>
+			    		<div class="form-group">
+			    			<input class="btn btn-default btn-block" type="submit" value="uPOST!">
+			    		</div>
+			    	</form>
+			    </div>
+			    <div class="col-md-2 hidden-sm hidden-xs"></div>
+			</div>
 		</div>
 	</body>
 <?php endif;?>
