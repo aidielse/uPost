d72cdb90f69340fb386b603c5d3d14c36cc937b8
mikeogod <mikeogod@gmail.com>
2014-03-17 16:28:20 -0400

    added facebook and twitter logo

---
+++ alert.php
@@ -0,0 +1,10 @@
+<!-- An alert that provides debug information -->
+<div id="debug" class="alert alert-warning alert-dismissable" hidden>
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    <p>Something went wrong!</p>
+</div>
+<!-- An alert that provides login and register result (including logging in SNSs)-->
+<div id="user_alert" class="alert alert-warning alert-dismissable" hidden>
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    <p>Something went wrong!</p>
+</div>
