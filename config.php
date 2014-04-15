fd9b26bcf91719e0c006a0c272edd1626e4daa5b
aarons100 <aarons104@gmail.com>
2014-04-15 19:04:44 -0400

    fixed default host, changed 'localhost' to 'upost.websci'

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 	//phpuser1 is the username that is created by db-init.sql for this app.
 	$username = "phpuser1";
 	$password = "phppass";
-	$host = "localhost";
+	$host = "upost.websci";
 	//Facebook api information
 	$appid = "290187461143677";
 	$appsecret = "7da03a3fb3b68b3bb8bbb8e593754f40";
