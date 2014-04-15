361eab739d93e1b42ea4d92afd9b31b868944b76
Alucea <aarons104@gmail.com>
2014-04-15 18:54:33 -0400

    fixed redirect_uri in config.php

--- config.php
+++ config.php
@@ -14,7 +14,7 @@
 
 	//Get the current host and uri, so that browser redirection works correctly in different development environments
 
-	$fb=array("APPID"=>$appid, "REDIRECT_URI"=>"./login.php?sns=facebook", "APP_SECRET"=>$appsecret);
+	$fb=array("APPID"=>$appid, "REDIRECT_URI"=>"http://{$host}{$uri}/login.php?sns=facebook", "APP_SECRET"=>$appsecret);
 	$tw=array();
 	$gp=array();
 
