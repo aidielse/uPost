407db994f643e6f5c00791f644eb211edead77ff
Alucea <aarons104@gmail.com>
2014-04-18 14:21:51 -0400

    Added lots o comments to login.php

--- login.php
+++ login.php
@@ -5,91 +5,98 @@ require_once("dbconnect.php");
 if(!empty($_POST)) {
 	//if the user it attempting to log in with facebook
 	if($_POST["sns"]=="login with Facebook") {
-
+		//grab information from config.php
 		$APPID=$fb["APPID"];
 		$REDIRECT_URI=$fb["REDIRECT_URI"];
-
+		//redirect the user to facebook to login, we also pass our credentials to facebook
 		header("Location: https://www.facebook.com/dialog/oauth?client_id={$APPID}&redirect_uri={$REDIRECT_URI}&response_type=code&scope=publish_actions");
 	}
 	else {
 		echo "No sns is selected!";
 	}
 }
+//
 else {
-	
+	//once the user is verified with facebook and we have an app code
 	if($_GET["sns"]=="facebook") {
-
+		//get variables from config.php and the get request
 		$code=$_GET["code"];
 		$APPID=$fb["APPID"];
 		$REDIRECT_URI=$fb["REDIRECT_URI"];
 		$APP_SECRET=$fb["APP_SECRET"];
 
-		echo "wut";
+		//echo "wut";
 		//Get a short lived token
 		//phpinfo();
 		$c=curl_init("https://graph.facebook.com/oauth/access_token?client_id={$APPID}&redirect_uri={$REDIRECT_URI}&client_secret={$APP_SECRET}&code={$code}");
-		echo "curl initialized\n";
+		//echo "curl initialized\n";
+		//configure curl options so that curl_exec($c) returns the short term token as a string.
 		curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
 		curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
-		echo "about to run curl_exec\n";
+		//echo "about to run curl_exec\n";
+		//$ret holds the short term access token
 		$ret=curl_exec($c);
-
+		//if we couldn't get the short term token
 		if(!$ret)
 		{
-			echo "failed getting short-term access token\n";
+			//echo "failed getting short-term access token\n";
 			login_fail_redirect();
 			die();
-		}
-		else {
-			echo "succeeded getting short-term access token\n";
 		}
 		//echo "something happened";
 
 		//$ret has format access_token=xxxxxxxx&expire=xxxxxx
 		parse_str($ret, $q);
+		//parse the short term access token to seperate the expiration,
+		//store the short term access token in $access_token
 		$access_token=$q["access_token"];			
 		//Exchange the short-lived token for a long-lived access token
+		//change the curl url to include the short-term access token
 		curl_setopt($c, CURLOPT_URL, "https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id={$APPID}&client_secret={$APP_SECRET}&%20fb_exchange_token={$access_token}");
+		//execute the curl, store the long term access token in $ret
 		$ret=curl_exec($c);
+
+		//if $ret is null, curl_exec() didn't work
 		if(!$ret)
 		{
-			echo "Failed getting long-term access token\n";
+			//echo "Failed getting long-term access token\n";
 			login_fail_redirect();
 			die();
 		}
-		else {echo "succeeded getting long-term access token\n";}
+		//get the actual long-term access token from $ret
 		parse_str($ret, $q);
 		//The Facebook access token
 		$access_token=$q["access_token"];
 			
 		//Get App's access token
+		//change curl url setting so that we can get the app access token
 		curl_setopt($c, CURLOPT_URL, "https://graph.facebook.com/oauth/access_token?client_id={$APPID}&client_secret={$APP_SECRET}&grant_type=client_credentials");
+		//execute, store app access token in $ret
 		$ret=curl_exec($c);
 		if(!$ret)
 		{
-			//
-			echo "failed getting app access token";
-			die();
+			//echo "failed getting app access token";
+			//die();
 			login_fail_redirect();
 			die();
 		}
-		else { echo "succeeded getting app access token\n";}
-		
+
 		parse_str($ret, $q);
+		//save app access token into $app_access_token
 		$app_access_token=$q["access_token"];
 			
 		//Get the user's id
 		curl_setopt($c, CURLOPT_URL, "https://graph.facebook.com/debug_token?input_token={$access_token}&access_token={$app_access_token}");
 		curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
+		//user id gets saved in $ret
 		$ret=curl_exec($c);
 		if(!$ret)
 		{
-			echo "failed to get facebook user_id\n";
+			//echo "failed to get facebook user_id\n";
 			login_fail_redirect();
 			die();
 		}
-		else {echo "got facebook user_id\n";}	
 		//Get user_id from the decoded json object, and format it to a non-scientific string number
 		$userid=(json_decode($ret, true)["data"]["user_id"]);
 		$userid=(sprintf('%.0f', $userid));
@@ -101,16 +108,16 @@ else {
 		$ret=curl_exec($c);
 		if(!$ret)
 		{
-			echo "Failed to get username\n";
+		//	echo "Failed to get username\n";
 			login_fail_redirect();
 			die();
 		}
-		else { echo "got username\n";}
+		//Get username from the decoded json object
 		$username=json_decode($ret, true)["username"];
-		
+		//exit curl
 		curl_close($c);
 		//Store the access token and username in the database
- 		session_start();
+ 		//session_start();
  		if(true)//!isset($_SESSION["user"]))
  		{
  			
