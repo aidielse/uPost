e0769fb3b594220dbd724bf15ced6ddf15d3ca68
Alucea <aarons104@gmail.com>
2014-04-19 12:26:58 -0400

    added  variable back to config.php

--- config.php
+++ config.php
@@ -2,6 +2,7 @@
 	//this file tracks credentials for mysql and for facebook login.
 
 	//Facebook api information
+	$host= "upost.websci";
 	$appid = "290187461143677";
 	$appsecret = "7da03a3fb3b68b3bb8bbb8e593754f40";
 
