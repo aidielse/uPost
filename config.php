4c6be9f972292649d8422556dcad3194fb8c5b2a
louy2 <loganlyf@gmail.com>
2014-04-27 15:34:13 -0400

    Change callback url to fix login

--- config.php
+++ config.php
@@ -5,32 +5,32 @@
 
 	//this file for Twitter
 	require_once './twitter-api-php-client/twitteroauth/twitteroauth.php';
-	
+
 	//this file for Facebook
 	require_once 'facebook-php-sdk/src/facebook.php';
 
-	$host = "uPost.websci";
+	$host = "upost.heroku.com";
 	//Make it GLOBAL
-	define('HOST',"uPost.websci");
+	define('HOST',"upost.heroku.com");
 	$uri="";
 	//Facebook api information
-	$appid = "722200331145073";
-	$appsecret = "5b98d8acacdd0c144ab71197a344a87b";
+	$appid = "290187461143677";
+	$appsecret = "7da03a3fb3b68b3bb8bbb8e593754f40";
 	$fb=array("APPID"=>$appid, "REDIRECT_URI"=>"http://{$host}{$uri}/login.php?sns=facebook", "APP_SECRET"=>$appsecret);
-	
+
 	//Twitter api credentials
 	$consumer_key="yVqGgDv0G6KLLMRpgvKL5Fdft";
 	$consumer_secret="KtzAMX2LuTvabvFaUu8MeCgYIEyRnrS7qtLLlExtzNIRl9AqVg";
 	$auth_callback="http://{$host}{$uri}/login.php?sns=twitter";
-	
-	
+
+
 	//Google+ api credentials
 	$googleplus_developer_key="AIzaSyC6ggim_1iP8T_8Qz8DWjRo5peZabQkEnw";
 	$client = new Google_Client();
 	$client->setApplicationName("Google+ PHP uPost app");
-	$client->setClientId('983819609735-ueud5lh1r2e04f3klssqrqehqfagt9ls.apps.googleusercontent.com');
-	$client->setClientSecret('F3esmITnFr44WJNB91FyQTy_');
-	$client->setRedirectUri('http://localhost/uPost/login.php?sns=googleplus');
+	$client->setClientId('983819609735-4a2pfaagttqonbc69t82u9p2bhqgnch7.apps.googleusercontent.com');
+	$client->setClientSecret('DnzO7MU5ef9Ka3s-b-nhR6gU');
+	$client->setRedirectUri('https://upost.heroku.com/login.php?sns=googleplus');
 	$client->setDeveloperKey('AIzaSyC6ggim_1iP8T_8Qz8DWjRo5peZabQkEnw');
 	$plus = new Google_PlusService($client);
 	//Get the current host and uri, so that browser redirection works correctly in different development environments
